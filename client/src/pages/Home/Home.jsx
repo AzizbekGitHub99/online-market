939d147ce30cc05dc9f4446260dc78e6deae17d4
Azizbek <azizbekxotamaliyev@gmail.com>
2024-12-01 12:51:17 +0500

    azyzbe prodCard

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Home.scss'
+import "./Home.scss";
+import ProdCard from "../../components/Cards/ProdCard/ProdCard";
 
 const Home = () => {
   return (
     <main>
       <section className="hero">
-
+        <ProdCard />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
